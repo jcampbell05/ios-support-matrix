74781cd50bd4c6982fc57ccdcaf300a9faef7db2
James Campbell <james@supmenow.com>
2016-07-01 12:44:35 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+
+module.exports = {
+  entry: './src/index.js',
+  output: { filename: 'main.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}

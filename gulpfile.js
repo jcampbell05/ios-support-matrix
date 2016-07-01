7bcf3ddf1cfb555878de7ae74e9c7663d1ae186b
James Campbell <james@supmenow.com>
2016-07-01 12:57:36 +0100

    Compile main.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var webpack = require('webpack-stream');
 
 gulp.task('default', function() {
   return gulp.src('src/index.js')
-  .pipe(webpack())
-  .pipe(gulp.dest('dist/index.js'));
+  .pipe(webpack({
+      output: {
+        filename: '[name].js',
+      },
+  }))
+  .pipe(gulp.dest('dist/'));
 });
